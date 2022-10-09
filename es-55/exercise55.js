2dc56274c8f1e5a191fa25226bfc3927d34124d1
Suleyman <suleyman20071998@gmail.com>
2022-10-09 22:08:29 +0300

    the ex 55 has done

---
+++ es-55/exercise55.js
@@ -0,0 +1,16 @@
+
+// # Classes - Exercise 55
+
+// Define a class called `Person` that takes in two arguments (`firstName` and `lastName`) in the constructor.
+
+// const developer = new Person('Mario', 'Rossi');
+// console.log(developer.firstName + " " + developer.lastName);
+class Person {
+  constructior(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+}
+
+const developer = new Person("Mario", "Rossi");
+console.log(developer.firstName + " " + developer.lastName);
